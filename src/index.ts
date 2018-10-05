e32d4e7d3183765bf948485d80a38c562ce8ab8c
Nathan Perry <nbp8557@rit.edu>
2018-10-04 21:00:06 -0400

    Adding tslint & prettier

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-const greeter = (name: string) => "Hello, " + name;
- const greet = greeter("Jake");
- console.log(greet);  
+const greeter = (name: string) => 'Hello, ' + name;
+
+const greet = greeter('Jake');
