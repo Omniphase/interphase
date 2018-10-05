26e1f0760eef6bb999592dd070c2acc8cd9a31d2
Dakota Bassett <djb5143@rit.edu>
2018-10-04 22:11:39 -0400

    Add client file.

---
+++ src/client/client.ts
@@ -0,0 +1,21 @@
+
+class HttpClient extends Client {
+  delete(path: string, requestData: RequestData, requestOptions: RequestOptions): ClientResponse<any> {
+
+  }
+  get(path: string, requestData: RequestData, requestOptions: RequestOptions): ClientResponse<any> {
+
+  }
+  patch(path: string, requestData: RequestData, requestOptions: RequestOptions): ClientResponse<any> {
+
+  }
+
+  post(path: string, requestData: RequestData, requestOptions: RequestOptions): ClientResponse<any> {
+
+  }
+
+  put(path: string, requestData: RequestData, requestOptions: RequestOptions): ClientResponse<any> {
+
+  }
+
+}
