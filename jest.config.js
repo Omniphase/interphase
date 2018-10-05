ac39d8b145ec504b705e9a0a9f20f01710450b48
Dakota Bassett <djb5143@rit.edu>
2018-10-04 20:52:26 -0400

    Fix config and use better test matcher.

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,22 @@
 module.exports = {
-  collectCoverageFrom: [
-    'src/**/*.ts',
-  ],
   projects: [
     {
+      collectCoverageFrom: [
+        'src/**/*.ts',
+      ],
       displayName: 'test-node',
       preset: 'ts-jest',
       testEnvironment: 'node',
+      testMatch: ['<rootDir>/test/**/*.test.ts'],
     },
     {
+      collectCoverageFrom: [
+        'src/**/*.ts',
+      ],
       displayName: 'test-browser',
       preset: 'ts-jest',
       testEnvironment: 'jsdom',
+      testMatch: ['<rootDir>/test/**/*.test.ts'],
     }
   ],
-  testMatch: ['./test/**/*.ts']
 }; 
